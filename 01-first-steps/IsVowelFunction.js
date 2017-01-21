9d9f65e9a50d15bb659f72eff33b5e26d1214577
JonasRodon <jonasrodon@gmail.com>
2017-01-21 16:15:26 +0100

    Add isVowel

--- 01-first-steps/IsVowelFunction.js
+++ 01-first-steps/IsVowelFunction.js
@@ -1,3 +1,12 @@
 // isVowel() function
-
 // Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
+function isVowel (chart) {
+  var r = /a|e|i|o|u/i
+  // console.log(chart.match(r))
+  if (chart.match(r)) {
+    return true
+  } else {
+    return false
+  }
+}
+isVowel('b')
